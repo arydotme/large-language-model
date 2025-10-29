9cdf701eb5e048d7b2dd9ed01eab2ddb2917c206
Ahmad Bukhari <ahmadbukhari100703@gmail.com>
2025-10-29 15:23:26 +0800

    add traceable in localLama.py file

--- chatbot/localLama.py
+++ chatbot/localLama.py
@@ -1,43 +1,39 @@
-"""
+'''
 Learn to create Large Language Model (LLM)
 
-This LLM using LLaMA 3.2:1b and LangSmith for monitoring
-"""
+This LLM using LLaMA 3.2 and LangSmith for monitoring
+'''
 
 # Import library
-import streamlit as st
-import os
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 from langsmith import traceable
+import streamlit as st
+import os
 
 load_dotenv()
 
-os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2", "true")
-os.environ["LANGCHAIN_ENDPOINT"] = os.getenv("LANGCHAIN_ENDPOINT", "https://api.langchain.org")
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGSMITH_PROJECT"] = os.getenv("LANGSMITH_PROJECT", "default")
-
+os.environ['LANGCHAIN_TRACING_V2'] = os.getenv('LANGCHAIN_TRACING_V2', 'true')
+os.environ['LANGCHAIN_ENDOPOINT'] = os.getenv('LANGCHAIN_ENDPOINT', 'https://api.smith.langchain.com')
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
 
 # Prompt Template
 prompt = ChatPromptTemplate.from_messages(
     [
-        (
-            "system",
-            "You are a helpful assistant. Please respond to the user's request only based on the given context.",
-        ),
-        ("user", "Question: {question}"),
+        ("system", "You are a helpful assistant. Please respond to the user's request only based on the given context."),
+        ("user", "Question: {question}")
     ]
 )
 
 # Streamlit framework
-st.title("Langchain with LLaMA 3.2")
+st.title('Langchain with LLaMA 3.2')
 input_text = st.text_input("What topic u want search")
 
 # Ollama LLaMA LLM
-model = ChatOllama(model="llama3.2:1b")
+model = ChatOllama(model = 'llama3.2:1b')
 output_parser = StrOutputParser()
 chain = prompt | model | output_parser
 
@@ -52,6 +48,5 @@ The @traceable decorator is a simple way to log traces from the LangSmith Python
 def run_chain(question):
     return chain.invoke({"question": question})
 
-
 if input_text:
-    st.write(run_chain(input_text))
+    st.write(chain.invoke({ 'question': input_text}))
